9bd28196c38ae8a4e81cf88e8eb9e6d101a29131
sebspetersen <sebspetersen@gmail.com>
2024-10-01 16:04:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def get_answer():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
