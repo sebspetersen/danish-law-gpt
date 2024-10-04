f93c279b901da48e6b23edbbecb244567f460339
sebspetersen <sebspetersen@gmail.com>
2024-10-04 11:44:03 +0200

    Update ChatWindow.js

--- frontend/src/components/ChatWindow.js
+++ frontend/src/components/ChatWindow.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import MessageItem from './MessageItem';
+import ChatInput from './ChatInput';
+
+function ChatWindow({ currentConversation, conversations, sendMessage }) {
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <h1>Danish Law Expert</h1>
+      </div>
+      <div className="chat-box">
+        {conversations[currentConversation].messages.map((message, index) => (
+          <MessageItem key={index} message={message} />
+        ))}
+      </div>
+      <ChatInput sendMessage={sendMessage} />
+    </div>
+  );
+}
+
+export default ChatWindow;
 
