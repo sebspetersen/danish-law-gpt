5930be13af91b4a855c1e2e9206672e606ec9ab4
sebspetersen <sebspetersen@gmail.com>
2024-10-04 11:44:08 +0200

    Update MessageItem.js

--- frontend/src/components/MessageItem.js
+++ frontend/src/components/MessageItem.js
@@ -1,1 +1,14 @@
+import React from 'react';
+
+function MessageItem({ message }) {
+  const { sender, content } = message;
+
+  return (
+    <div className={`message ${sender}`}>
+      <span>{content}</span>
+    </div>
+  );
+}
+
+export default MessageItem;
 
